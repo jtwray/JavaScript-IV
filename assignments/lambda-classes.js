ae70e0b47934e4238cb1e2031606286aa2292ee0
jtwray <tuckerwray84@gmail.com>
2019-04-25 19:53:44 -0400

    creates class Person(attributes){[name,age,location,gender]method:speak}

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,16 @@
 // CODE here for your Lambda Classes
+class Person {
+
+    constructor(attributes) {
+            this.createdAt = attributes.createdAt;
+            this.name = attributes.name;
+            this.age = attributes.age;
+            this.location = attributes.location;
+            this.gender = attributes.gender;
+
+        }
+        //methods
+    speak() {
+        return `Hello my name is ${this.name}, I am from ${this.location}.`;
+    };
+} //GameObject
