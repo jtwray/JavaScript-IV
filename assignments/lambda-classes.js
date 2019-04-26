9a1a18e258032bbe9c74a731205bcdc0326c207c
jtwray <tuckerwray84@gmail.com>
2019-04-25 20:05:14 -0400

    creates child of Parent, class Instructor

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -12,5 +12,21 @@ class Person {
         //methods
     speak() {
         return `Hello my name is ${this.name}, I am from ${this.location}.`;
-    };
-} //GameObject
+    }
+} //Person
+
+class Instructor extends Person {
+    constructor(childAttributes) {
+            super(childAttributes);
+            this.specialty = childAttributes.specialty;
+            this.favLanguage = childAttributes.favLanguage;
+            this.catchPhrase = childAttributes.catchPhrase;
+        }
+        //methods
+    demo(subject) {
+        return `Today we are learning about ${subject}.`;
+    }
+    grade(student, subject) {
+        `${student.name} recieves a perfect score on ${subject}.`;
+    }
+}
